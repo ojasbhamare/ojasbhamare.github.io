f738c6d3cbe10da6f6bddd0e421775dde4da2721
ojasbhamare <ojasbhamare@users.noreply.github.com>
2020-05-02 13:59:31 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -4,16 +4,16 @@
 #include<ctype.h>
 #include<cs50.h>
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
-    if (argc !=2)
+    if (argc != 2)
     {
         printf("Usage: ./caesar key");
         return 1;
     }
-    int x= atoi(argv[1]);
+    int x = atoi(argv[1]);
 
-    for(int i=0;i<strlen(argv[1]);i++)
+    for (int i = 0 ; i < strlen(argv[1]) ; i++)
     {
         if (! isdigit(argv[1][i]))
         {
@@ -23,17 +23,16 @@ int main(int argc,char *argv[])
     }
     string s = get_string("plaintext: ");
 
-    for(int i=0;i<strlen(s);i++)
+    for (int i = 0 ; i < strlen(s) ; i++)
     {
-        if(islower(s[i]))
+        if (islower(s[i]))
         {
-            s[i]=97+(s[i]-97+x)%26;
+            s[i] = 97 + (s[i] - 97 + x) % 26;
         }
-        if(isupper(s[i]))
+        if (isupper(s[i]))
         {
-            s[i]=65+(s[i]-65+x)%26;
-
+            s[i] = 65 + (s[i] - 65 + x) % 26;
         }
     }
-    printf("ciphertext: %s\n",s);
+    printf("ciphertext: %s\n", s);
 }
