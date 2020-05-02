3c5121ad5c81f6a240b12d1b3a0289d55c86011d
ojasbhamare <ojasbhamare@users.noreply.github.com>
2020-05-02 16:54:35 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -1,5 +1,13 @@
 while True:
-    n = int(input('Height: '))
+    n = input('Height: ')
+    if n == '':
+        continue
+    for i in n:
+        if n not in range(0, 10):
+            pass
+    else:
+        continue
+    n = int(n)
     if 0 < n < 9:
         break
 
