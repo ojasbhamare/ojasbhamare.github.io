6f9bc276f29f6cadd91b6cf166d734773fb1121f
ojasbhamare <ojasbhamare@users.noreply.github.com>
2020-05-02 17:08:49 +0000

    automated commit by submit50

---
+++ mario.py
@@ -0,0 +1,25 @@
+while True:
+    n = input('Height: ')
+    if n == '':
+        continue
+    z = 'int'
+    for i in n:
+        if i not in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']:
+            z = 'notint'
+    if z == 'notint':
+        continue
+    n = int(n)
+    if 0 < n < 9:
+        break
+
+for i in range(n):
+    for j in range(n):
+        if j >= n - i - 1:
+            print('#', end='')
+        else:
+            print(' ', end='')
+    print(end='  ')
+    for k in range(n):
+        if k <= i:
+            print('#', end='')
+    print()
