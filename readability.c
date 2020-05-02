2789cb739ad486b1995b13baffea42d358b8fcbb
ojasbhamare <ojasbhamare@users.noreply.github.com>
2020-05-02 15:47:10 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -3,42 +3,49 @@
 #include<string.h>
 #include<math.h>
 
+int roundNo(float num)
+{
+    return num < 0 ? num - 0.5 : num + 0.5;
+}
+
 int main(void)
 {
-    int l, w, s = 0;
-    string text =get_string("Text: ");
+    int l=0;
+    int w=0;
+    int s=0;
+    string text = get_string("Text: ");
 
-    for(int i=0;i<strlen(text);i++)
+    for (int i = 0; i < strlen(text) ; i++)
     {
-        if(text[i]=='.'||text[i]=='!'||text[i]=='?')
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
         {
-            s+=1;
+            s += 1;
         }
     }
-    for(int i=0;i<strlen(text);i++)
+    for (int i = 0 ; i < strlen(text) ; i++)
     {
-        if(text[i]==' ')
+        if (text[i] == ' ')
         {
-            w+=1;
+            w += 1;
         }
     }
-    w+=1;
+    w += 1;
 
-    for(int i=0;i<strlen(text);i++)
+    for (int i = 0 ; i < strlen(text) ; i++)
     {
-        if('a'<=text[i]<='z'||'A'<=text[i]<='Z')
+        if ((65 <= (int) text[i] && (int) text[i] <= 90 )|| (97 <=(int) text[i] && (int) text[i] <= 122))
         {
-            l+=1;
+            l += 1;
         }
     }
 
-    float ans=round(5.88*(l/w)-29.6*(s/w)-15.8);
+    int ans = roundNo(5.88 * (l/w) - 29.6 * (s/w) - 15.8);
 
-    if (ans<1)
+    if (ans < 1)
     {
         printf("Before Grade 1");
     }
-    else if (ans>16)
+    else if (ans > 16)
     {
         printf("Grade 16+");
     }
