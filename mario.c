6c3b084eda999ba3fe54488ac7040fdc450fd197
ojasbhamare <ojasbhamare@users.noreply.github.com>
2020-05-02 13:34:31 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -6,13 +6,14 @@ int main(void)
     int x;
     do                         //getting int between 1 and 8
     {
-        x =get_int("Height: ");
-    } while (x<1 ||x>8);
-    for(int i=0;i<x;i++)
+        x = get_int("Height: ");
+    }
+    while (x < 1 || x > 8);
+    for (int i = 0; i < x ; i++)
     {
-        for(int j=0;j<x;j++)   //Left part of objective
+        for (int j = 0; j < x; j++)   //Left part of objective
         {
-            if (j>=x-i-1)
+            if (j >= x - i - 1)
             {
                 printf("#");
             }
@@ -24,7 +25,7 @@ int main(void)
 
         printf("  ");            //Middle gap on each line
 
-        for(int k=0;k<i+1;k++)  //Right part of objective
+        for (int k = 0; k < i + 1; k++) //Right part of objective
         {
             printf("#");
         }
