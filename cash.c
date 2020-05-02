1e01ed63232e2c3c32c290ff3f148c3d2fb35fe6
ojasbhamare <ojasbhamare@users.noreply.github.com>
2020-05-02 13:40:57 +0000

    automated commit by check50 [check50=True]

--- cash.c
+++ cash.c
@@ -7,21 +7,21 @@ int main(void)
     float x;
     do
     {
-        x= get_float("Change owed: ");
+        x = get_float("Change owed: ");
     }
-    while (x<0);
-    x=round(x*100);
-    int ans=0;
-    int lst[4]={25,10,5,1};
-    for(int i=0;i<4;i++)
+    while (x < 0);
+    x = round(x * 100);
+    int ans = 0;
+    int lst[4] = {25, 10, 5, 1};
+    for (int i = 0 ; i < 4 ; i++)
     {
-        int j=lst[i];
-        while (x>=j)
+        int j = lst[i];
+        while (x >= j)
         {
-            x-=j;
-            ans+=1;
+            x -= j;
+            ans += 1;
         }
     }
-    printf("%i\n",ans);
+    printf("%i\n", ans);
 
 }
