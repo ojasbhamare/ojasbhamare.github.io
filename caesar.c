b97da504e3a2a0669b9d1a15884c18c1126c94cd
ojasbhamare <ojasbhamare@users.noreply.github.com>
2020-05-02 13:51:16 +0000

    automated commit by check50 [check50=True]

---
+++ caesar.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+#include<cs50.h>
+
+int main(int argc,char *argv[])
+{
+    if (argc !=2)
+    {
+        printf("Usage: ./caesar key");
+        return 0;
+    }
+    int x= atoi(argv[1]);
+
+    for(int i=0;i<strlen(argv[1]);i++)
+    {
+        if (! isdigit(argv[1][i]))
+        {
+            printf("Usage: ./caesar key");
+            return 0;
+        }
+    }
+    string s = get_string("plaintext: ");
+
+    for(int i=0;i<strlen(s);i++)
+    {
+        if(islower(s[i]))
+        {
+            s[i]=97+(s[i]-97+x)%26;
+        }
+        if(isupper(s[i]))
+        {
+            s[i]=65+(s[i]-65+x)%26;
+
+        }
+    }
+    printf("ciphertext: %s",s);
+}
