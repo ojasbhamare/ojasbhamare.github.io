a4e0ba747b834d342fd27edec8721e5287949f6e
ojasbhamare <ojasbhamare@users.noreply.github.com>
2020-05-14 09:42:23 +0000

    automated commit by submit50

--- import.py
+++ import.py
@@ -1,4 +1,6 @@
-import sys,csv,cs50
+import sys
+import csv
+import cs50
 
 if len(sys.argv) != 2:
     print("Error")
@@ -6,16 +8,15 @@ if len(sys.argv) != 2:
 
 db = cs50.SQL("sqlite:///students.db")
 
-with open (sys.argv[1], "r") as houses:
+with open(sys.argv[1], "r") as houses:
 
     reader = csv.reader(houses)
 
     for row in reader:
         name = row[0].split()
         if len(name) == 3:
-            db.execute("INSERT INTO houses (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)", name[0], name[1], name[2], row[1] ,row[2])
+            db.execute("INSERT INTO houses (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)",
+                       name[0], name[1], name[2], row[1], row[2])
         elif len(name) == 2:
-            db.execute("INSERT INTO houses (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)", name[0], None, name[1], row[1] ,row[2])
-
-
-
+            db.execute("INSERT INTO houses (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)",
+                       name[0], None, name[1], row[1], row[2])
